31357605ec7a199377f2e4fe448d75067e123628
U-PS11-03\vantan <vantan@PS11-03.(none)>
2013-12-14 17:32:34 +0900

    added

--- memo.c
+++ memo.c
@@ -1,17 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define BUF_SIZE 1024
 
+enum tokType
+{
+	OpPlus,OpMinus,OpEqual,
+	Number
+};
+
+int getType(char c)
+{
+	enum tokType type;
+	
+	if(c == '+')
+	{
+		type = OpPlus;
+	}
+	if(c == '-')
+	{
+		type = OpMinus;
+	}
+	if(c == '=')
+	{
+		type = OpEqual;
+	}
+	if(isdigit(c))
+	{
+		type = Number;
+	}
+	return type;
+}
+
 int main()
 {
-	FILE *fp;
+	
+	int c;
+	while((c=getchar()) != EOF)
+	{
+		if(isspace(c))
+		{
+			continue;
+		}
+		if(c == '+' || c == '-' || c == '=')
+		{
+			putchar(c);
+			break;
+		}
+		else if(isdigit(c))
+		{
+			do
+			{
+				putchar(c);
+			}
+			while((c = getchar()) != EOF && isdigit(c));
+			break;
+		}
+		else
+		{
+			fprintf(stderr,"invalied char %c\n", c);
+		}
+	}
+	
+	/*FILE *fp;
 	char c[BUF_SIZE];
 	
 	printf("nyuuryokusiro\n");
 	scanf("%c",c);
 	printf("%c",c[0]);
-	/*fp = fopen("A.txt","r");
+	
+	fp = fopen("A.txt","r");
 	
 	if(fp == NULL)
 	{
@@ -19,8 +78,9 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 	c = fgetc(fp);
-	printf("%c",c);*/
+	printf("%c",c);
 	
-	fclose(fp);
+	fclose(fp);*/
+	
 	return 0;
 }
